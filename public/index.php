c703ad669c503c5d8c0d4727f9b9fdcd32fccb5e
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2017-09-30 00:39:36 +0200

    creating project structure

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$test = new \GSoares\CweTop25\Cwe89\Wrong();
---
+++ src/CweTop25/Cwe89/Wrong.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GSoares\CweTop25\Cwe89;
+
+class Wrong
+{
+    public function weakQuery($externalParameter)
+    {
+
+        /*
+        string userName = ctx.getAuthenticatedUserName();
+        string query = "SELECT * FROM items WHERE owner = '" + userName + "' AND itemname = '" + ItemName.Text + "'";
+        sda = new SqlDataAdapter(query, conn);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        */
+    }
+}
