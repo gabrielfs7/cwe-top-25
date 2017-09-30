5476ba1e2e9c115cfbdd7888ccc368e024d721cd
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2017-09-30 13:05:18 +0200

    create navigation for cwe weakness

---
+++ src/CweTop25/Application/Controller/CweController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GSoares\CweTop25\Application\Controller;
+
+/**
+ * @author Gabriel Felipe Soares <gabrielfs7@gmail.com>
+ */
+class CweController extends AbstractController
+{
+
+    /**
+     * @param string $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction($id)
+    {
+        return $this->renderResponse(
+            "cwe/$id.html.twig",
+            [
+
+            ]
+        );
+    }
+}
